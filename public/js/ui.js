f578a2018eda27e4d54e8f7156d9ee89708ba911
axquaris <dominykas@berkeley.edu>
2023-12-20 18:59:10 -0800

    semi-working

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,19 +1,27 @@
-// import { html, css, LitElement } from 'https://unpkg.com/lit@2.0.2?module';
 import { LitElement, html, css } from 'https://unpkg.com/lit@2.0.2?module';
 
 var dynamicVariable = 0;
 
+const shared_styles = css`
+    :host {
+        --bs-gutter-x: 1.5rem;
+        --bs-gutter-y: 0;
+        margin: 0px;
+        padding: 0px;
+    }
+
+    .container {
+        background-color: #1c232c;
+        border-radius: 5px;
+        padding-left: 5px;
+        margin-top: 5px;
+        height: 160px;
+    }
+`
+
 // Define a new custom HTML element called "building-slot"
 class BuildingSlot extends LitElement {
-    static styles = css`
-        .container {
-            background-color: #1c232c;
-            border-radius: 5px;
-            padding: 5px;
-            margin-top: 5px;
-            height: 160px;
-        }
-    `;
+    static styles = shared_styles;
 
     constructor() {
         super();
@@ -29,7 +37,7 @@ class BuildingSlot extends LitElement {
                 <div class="row">
                     <div class="col-6">
                         <h3>Supply Plant</h3>
-                        <p>${dynamicVariable}</p>
+                        ${dynamicVariable}
                     </div>
                     <div class="col-6">
                         <button id="myButton" @click=${this.handleClick}>Click Me</button>
@@ -49,16 +57,7 @@ class BuildingSlot extends LitElement {
 
 // Define a new custom HTML element called "building-slot"
 class OutpostSlot extends LitElement {
-    static styles = css`
-        .container {
-            text-align: left;
-            background-color: #1c232c;
-            border-radius: 5px;
-            padding: 5px;
-            margin-top: 5px;
-            height: 160px;
-        }
-    `;
+    static styles = shared_styles;
 
     constructor() {
         super();
@@ -74,7 +73,7 @@ class OutpostSlot extends LitElement {
                 <div class="row">
                     <div class="col-6">
                         <h3>Supply Plant</h3>
-                        <p>${dynamicVariable}</p>
+                        ${dynamicVariable}
                     </div>
                     <div class="col-6">
                         <button id="myButton" @click=${this.handleClick}>Click Me</button>
@@ -95,4 +94,4 @@ class OutpostSlot extends LitElement {
 customElements.define('building-slot', BuildingSlot);
 customElements.define('outpost-slot', OutpostSlot);
 
-export {dynamicVariable}
+export { dynamicVariable }
